0aca4017e84a5d34eda760de1d26e65cae68f365
Danielkvist <danielkvist@protonmail.com>
2020-05-09 10:06:36 +0200

    use the Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -18,6 +19,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // To fix overflow we could hardcore the width
+    // but that is a code smell.
+    // width: 200.0.
+    // A better solution is to use Expanded.
+    //
+    // The Expanded widget will resize to fill
+    // the available horizontal/vertical space
+    // if you don't specify how much it should
+    // grow with:
+    //    * flex (by default is set to 1).
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
