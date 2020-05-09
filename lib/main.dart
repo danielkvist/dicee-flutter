a9d65d6b1b119b4a456ae3db3238697dd2febe88
Danielkvist <danielkvist@protonmail.com>
2020-05-09 10:28:27 +0200

    center images

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // To fix overflow we could hardcore the width
-    // but that is a code smell.
-    // width: 200.0.
-    // A better solution is to use Expanded.
-    //
-    // The Expanded widget will resize to fill
-    // the available horizontal/vertical space
-    // if you don't specify how much it should
-    // grow with:
-    //    * flex (by default is set to 1).
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
